7faa1f7b4d4dbba9e07c86a7ef1f5bd79af2a59d
hcp <suna.hing@gmail.com>
2018-10-15 20:39:54 +0900

    no message

--- Assets/MapLoadingPart/02.Scripts/Map/MapPathGenerator.cs
+++ Assets/MapLoadingPart/02.Scripts/Map/MapPathGenerator.cs
@@ -5,13 +5,16 @@ namespace hcp
 {
     public class MapPathGenerator : SingletonTemplate<MapPathGenerator>
     {
+        IMapTurnToUI mapTurnToUI;
+        GameObject ui;
+
         int frontShowChunk;
         float margin;
         public float turnProbability=50;   //회전 확률
         int turningTerm = 10;
 
         public float GetTurnProbability() { return turnProbability; }
-        public void SetGetTurnProbability(float value)
+        public void SetTurnProbability(float value)
         {
             if (value >= 100) turnProbability = 100;
             else
@@ -20,6 +23,7 @@ namespace hcp
             }
         }
 
+        
 
         // Use this for initialization
         protected override void Awake()
@@ -27,18 +31,28 @@ namespace hcp
             base.Awake();
             frontShowChunk = MapObjManager.GetInstance().wantToShowNumOfChunks 
                 - MapObjManager.GetInstance().wantToShowNumOfChunksInBehind;
+
+            mapTurnToUI = GameObject.Find("TController").GetComponent<IMapTurnToUI>();
+            ui = GameObject.Find("TController");
         }
+
         private void Start()
         {
             margin = MapObjManager.GetInstance().GetChunkMargin();
+
+            TouchControl.swipeScreen += new SwipeScreen( tempDeEvent);
+
+        }
+        void tempDeEvent(float a, E_WhichTurn whichTurn) {
+            Debug.Log("델리게이트 이벤트 호출 받음 매개변수 =" + a + whichTurn);
         }
 
         public float WillTurn(float nowPos,float turnedPoint , out E_WhichTurn whichTurn,out Vector3 turnChunksPos)  //터닝포인트 (10단위 위치) 리턴
          {
             whichTurn = E_WhichTurn.NOT_TURN;
             turnChunksPos = Vector3.zero;
-            Debug.Log("터닝텀은 = "+(turnedPoint + turningTerm * MapObjManager.GetInstance().GetChunkMargin()));
-            if (turnedPoint + turningTerm * MapObjManager.GetInstance().GetChunkMargin() > nowPos) //터닝 텀.
+            Debug.Log("터닝텀은 = "+(turnedPoint + turningTerm * margin));
+            if (turnedPoint + turningTerm * margin > nowPos) //터닝 텀.
                 return 0;
 
             float turning = Random.Range(0f, 100f);   //방향전환확률
@@ -49,7 +63,7 @@ namespace hcp
                 print("터닝 플래그 온");
 
                 float makePos = nowPos + frontShowChunk * margin;
-                float turningPoint = makePos+(2 * margin);
+                float turningPoint = makePos+(2 * margin);  //2는 기역자 청크모델 자체에 의해 결정된 값
                 turnChunksPos = new Vector3(0, 0, makePos);
 
                 print("생성위치 ="+ turnChunksPos + "터닝포인트="+turningPoint);
@@ -66,6 +80,10 @@ namespace hcp
                     whichTurn = E_WhichTurn.RIGHT;
                     //오른쪽 청크 생성
                 }
+                //ui 쪽에 터닝포인트와 방향 알려줌
+                mapTurnToUI.SetTurningPointToUI(turnedPoint);
+                mapTurnToUI.SetWhichTurnToUI(whichTurn);
+
                 return turningPoint;
             }
         }
