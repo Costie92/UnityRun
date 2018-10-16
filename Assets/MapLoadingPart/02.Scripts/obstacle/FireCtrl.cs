01839100c0f6b686b1f5cead803de3f0f5865298
hcp <suna.hing@gmail.com>
2018-10-16 17:50:39 +0900

    no message

---
+++ Assets/MapLoadingPart/02.Scripts/obstacle/FireCtrl.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace hcp
+{
+    public class FireCtrl : ObstacleCtrl
+    {
+
+        public override void FromChildOnCollisionEnter(GameObject child, Collision coll)
+        {
+            Debug.Log("FIRE 장애물 콜리전엔터 이벤트");
+            if (coll.gameObject.CompareTag("PLAYER"))
+            {
+                objToCharactor.BeenHitByObs(obsST);
+
+            }
+            obsST.beenHit = false;
+        }
+
+        public override void FromChildOnTriggerEnter(GameObject child, Collider other)
+        {
+            Debug.Log("FIRE 장애물 트리거엔터 이벤트");
+            if (other.gameObject.CompareTag("PLAYER"))
+            {
+                objToCharactor.BeenHitByObs(obsST);
+
+            }
+            obsST.beenHit = false;
+        }
+        // Use this for initialization
+        protected override void Awake()
+        {
+            base.Awake();
+            obsST.obstacleType = E_OBSTACLE.FIRE;
+        }
+
+    }
+}
