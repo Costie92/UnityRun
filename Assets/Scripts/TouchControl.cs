90a4af5c1055b8d29c0333d84a2ebe042ad193a4
Costie <costie92@gmail.com>
2018-10-18 20:36:22 +0900

    UI Management

--- Assets/Scripts/TouchControl.cs
+++ Assets/Scripts/TouchControl.cs
@@ -45,75 +45,82 @@ public class TouchControl : MonoBehaviour,IMapTurnToUI {
     // Update is called once per frame
     void Update()
     {
-        if (ObjEat.Invincible) {
+        if (ObjEat.Invincible)
+        {
             SwipeToTurn();
         }
         //마우스 클릭시 포지션 저장
-        if (Input.GetMouseButtonDown(0)) {
-            if (Input.mousePosition.y > (height / 2))
-            {
-                ButtonDownMousePos = Input.mousePosition;
-                TapCount = 1;
-            }
-        }
-        // 마우스 클릭 유지시
-        if (Input.GetMouseButton(0))
+        if (!UIManager.isPause)
         {
-            MousePosX = Input.mousePosition.x;
-            MousePosY = Input.mousePosition.y;
-            //하단 부분 클릭 했을 경우
-            if (TapCount == 0)
+            if (Input.GetMouseButtonDown(0))
             {
-                if (MousePosY < (height / 2))
+                if (Input.mousePosition.y > (height / 2))
                 {
-                    //좌측으로 이동
-                    if (MousePosX < (width / 2))
+                    ButtonDownMousePos = Input.mousePosition;
+                    TapCount = 1;
+                }
+            }
+            // 마우스 클릭 유지시
+            if (Input.GetMouseButton(0))
+            {
+                MousePosX = Input.mousePosition.x;
+                MousePosY = Input.mousePosition.y;
+                //하단 부분 클릭 했을 경우
+                if (TapCount == 0)
+                {
+                    if (MousePosY < (height / 2))
                     {
-                        cMove.turningPoint = false;
-                        cMove.Move(true);
-                    }
-                    //우측으로 이동
-                    else
-                    {
-                        cMove.turningPoint = false;
-                        cMove.Move(false);
+                        //좌측으로 이동
+                        if (MousePosX < (width / 2))
+                        {
+                            cMove.turningPoint = false;
+                            cMove.Move(true);
+                        }
+                        //우측으로 이동
+                        else
+                        {
+                            cMove.turningPoint = false;
+                            cMove.Move(false);
+                        }
                     }
                 }
             }
-        }
-        //마우스 클릭 종료시
-        else if (Input.GetMouseButtonUp(0)) {
-            //좌로 스와이프
-            if (TapCount == 1) {
-                if (ButtonDownMousePos.x - MousePosX > (width / 5))
+            //마우스 클릭 종료시
+            else if (Input.GetMouseButtonUp(0))
+            {
+                //좌로 스와이프
+                if (TapCount == 1)
                 {
-                    SetWhichTurnToUI(E_WhichTurn.LEFT);
-                    print("Swipe Left");
-                    SwipeToTurn();
+                    if (ButtonDownMousePos.x - MousePosX > (width / 5))
+                    {
+                        SetWhichTurnToUI(E_WhichTurn.LEFT);
+                        print("Swipe Left");
+                        SwipeToTurn();
 
 
-                }
-                //우로 스와이프
-                else if (MousePosX - ButtonDownMousePos.x > (width / 5))
-                {
-                    SetWhichTurnToUI(E_WhichTurn.RIGHT);
-                    print("Swipe Right");
-                    SwipeToTurn();
-                }
-                //위로 스와이프
-                else if (MousePosY - ButtonDownMousePos.y > (height / 10))
-                {
-                    cMove.Jump();
-                    print("Swipe Up");
+                    }
+                    //우로 스와이프
+                    else if (MousePosX - ButtonDownMousePos.x > (width / 5))
+                    {
+                        SetWhichTurnToUI(E_WhichTurn.RIGHT);
+                        print("Swipe Right");
+                        SwipeToTurn();
+                    }
+                    //위로 스와이프
+                    else if (MousePosY - ButtonDownMousePos.y > (height / 10))
+                    {
+                        cMove.Jump();
+                        print("Swipe Up");
 
+                    }
+                    //아래로 스와이프
+                    else if (ButtonDownMousePos.y - MousePosY > (height / 10))
+                    {
+                        cMove.SlideDown();
+                        print("Swipe Down");
+                    }
+                    TapCount = 0;
                 }
-                //아래로 스와이프
-                else if (ButtonDownMousePos.y - MousePosY > (height / 10))
-                {
-                    cMove.SlideDown();
-                    print("Swipe Down");
-                }
-                TapCount = 0;
             }
         }
     }
