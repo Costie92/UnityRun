4a26f255f43c3585666353dbe69827f0ade91c0f
HGS <cats.c.cats@gmail.com>
2018-10-19 17:46:27 +0900

    no message

--- Assets/Scripts/ObjEat.cs
+++ Assets/Scripts/ObjEat.cs
@@ -3,29 +3,46 @@ using System.Collections.Generic;
 using UnityEngine;
 
 using hcp;
-public class ObjEat : MonoBehaviour,IObjToCharactor { // ,IObjToCharactor
+public class ObjEat : MonoBehaviour, IObjToCharactor
+{ // ,IObjToCharactor
 
     Collision collision;
     public static int Coin = 0; // 동전수
     public static int HP = 3; // 체력 //처음3개 최대5개
-    bool speedUpItem = false;
-    private float attacked = 20.0f;
     public static bool Invincible = false;
+    public static float invincibleTime = 0.0f;
     public static bool Shield = false;
+    public static float shieldTime = 0.0f;
     public static bool Magnet = false;
+    public static float magnetTime = 0.0f;
+    bool iOverlap, sOverlap ,mOverlap = false ; // 방어막 겹쳤을때 확인 mOverlap(자석)은 문제없으므로 안쓰임
 
     // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if(HP < 0)
+    void Start()
+    {
+
+        HP = 3;
+        Invincible = false;
+        invincibleTime = 0.0f;
+        Shield = false;
+        shieldTime = 0.0f;
+        Magnet = false;
+        magnetTime = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (HP < 0)
         {
             HP = 0;
         }
-	}
+        ShieldEvent();
+        MagnetEvent();
+        InvicibleEvent();
+        InvicibleEventOn();
+        InvicibleEventOff();
+    }
 
     public void GetItem(ItemST itemST) //아이템얻었을때
     {
@@ -39,29 +56,40 @@ public class ObjEat : MonoBehaviour,IObjToCharactor { // ,IObjToCharactor
                 Debug.Log("HP " + HP + " 개");
                 break;
             case E_ITEM.INVINCIBLE: //무적
+                invincibleTime = 5;
+                Invincible = true;
+                StartCoroutine(InvincibleCoroutine());
                 Debug.Log("INVINCIBLE 먹음");
-                if (Invincible == false)
-                {
-                    Invincible = true;
-                    INVINCIBLEItem();
-                }
                 break;
             case E_ITEM.SHIELD: //방어막먹음
-                ShieldEvent();
+                shieldTime = 10;
+                Shield = true;
+                StartCoroutine(ShieldCoroutine());
+                if (Shield == true)
+                {
+                    sOverlap = true;
+                    Invoke("ShieldEvent3", 1.0f);
+                }
+                if(Shield == false)
+                {
+                    ShieldEvent();
+                }
                 Debug.Log("SHIELD 먹음");
                 break;
             case E_ITEM.COIN: //동전먹음
                 Coin++; // 동전수 +1
-                Debug.Log("COIN " + Coin +" 개");
                 break;
             case E_ITEM.MAGNET: //자석먹음
+                magnetTime = 5;
+                Magnet = true;
+                StartCoroutine(MagnetCoroutine());
                 MagnetEvent();
                 Debug.Log("MAGNET 먹음");
                 break;
             case E_ITEM.EITEMMAX:
                 Debug.Log("아이템 겟에서 오류");
                 return;
-               
+
         }
     }
 
@@ -83,7 +111,7 @@ public class ObjEat : MonoBehaviour,IObjToCharactor { // ,IObjToCharactor
                 Debug.Log("UPPER_HUDDLE");
                 break;
             case E_OBSTACLE.FIRE:
-                if(Invincible == false)
+                if (Invincible == false)
                 {
                     if (Shield == false)
                     {
@@ -99,60 +127,79 @@ public class ObjEat : MonoBehaviour,IObjToCharactor { // ,IObjToCharactor
         }
     }
 
-    void MagnetEvent()
-    {
-        Magnet = true;
-        Invoke("MagentDestroy", 10.0f);
-    }
-
-    void MagentDestroy()
-    {
-        print("자석 끝");
-        Magnet = false;
-    }
-
     void ShieldEvent()
     {
-        Shield = true;
-        Invoke("ShieldDestroy", 10.0f);
-    }
-
-    void ShieldDestroy()
-    {
-        print("실드 자연파괴");
-        Shield = false;
-    }
-
-    void INVINCIBLEItem() // 스피드업 아이템 기능구현 함수
-    {
-        Debug.Log("스피드업 아이템 먹음");
-        CharacterMove.runSpeed *= 5f; // 달리기 5배
-        Invoke("INVINCIBLE", 3.5f); // 스피드업 기능을 3.5f시간만큼 지속
-    }
-
-    void INVINCIBLE() // 스피드업 기능
-    {
-       this.GetComponent<CapsuleCollider>().isTrigger = true; // 오브젝트 뚫고가기
-       this.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionY; // Rigidbody Y포지션 고정
-        CharacterMove.runSpeed = CharacterMove.runSpeed / 5f;
-        Invoke("INVINCIBLEOff", 0.1f);
-    }
-
-    void INVINCIBLEOff() // 스피드업 기능
-    {
-        this.GetComponent<CapsuleCollider>().isTrigger = false; // 오브젝트 뚫기 해제
-        this.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None; // Rigidbody 포지션 초기화
-        Invincible = false;
-    }
-
-    void DamagedEvent()
-    {
-        if(Invincible == false && Shield == true)
+        if(shieldTime > 0)
         {
-            print("실드 없어짐");
-            Invoke("ShieldDestroy", 1.0f);
+            Shield = true;
         }
-        if(Invincible == false && Shield == false)
+        if(shieldTime <= 0)
+        {
+            Shield = false;
+        }
+    }
+
+    void ShieldEvent2()
+    {
+            shieldTime = 0;
+    }
+
+    void ShieldEvent3()
+    {
+        shieldTime = 9;
+    }
+
+    void MagnetEvent()
+    {
+        if (magnetTime > 0)
+        {
+            Magnet = true;
+        }
+        if (magnetTime <= 0)
+        {
+            Magnet = false;
+        }
+    }
+
+    void InvicibleEvent()
+    {
+        if (invincibleTime > 0)
+        {
+            Invincible = true;
+        }
+        if (invincibleTime <= 0)
+        {
+            Invincible = false;
+        }
+    }
+
+    void InvicibleEventOn() // 무적상태
+    {
+        if (Invincible == true)
+        {
+            CharacterMove.runSpeed = 32.5f;
+            this.GetComponent<CapsuleCollider>().isTrigger = true; // 오브젝트 뚫고가기
+            this.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionY; // Rigidbody Y포지션 고정
+        }
+    }
+
+    void InvicibleEventOff() // 무적아닌상태
+    {
+        if (Invincible == false)
+        {
+            CharacterMove.runSpeed = 6.5f;
+            this.GetComponent<CapsuleCollider>().isTrigger = false; // 오브젝트 뚫기 해제
+            this.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation; // Rigidbody 포지션 초기화
+        }
+    }
+
+    void DamagedEvent() // 장애물과 닿을때 이벤트
+    {
+        if (Invincible == false && Shield == true)
+        {
+                Invoke("ShieldEvent2", 1.0f);
+        }
+        if (Invincible == false && Shield == false)
         {
             DamagedEvent2();
         }
@@ -160,12 +207,12 @@ public class ObjEat : MonoBehaviour,IObjToCharactor { // ,IObjToCharactor
 
     void DamagedEvent2() // 적과 충돌시 발생하는 애니메이션
     {
-        if(HP > 1)
+        if (HP > 1)
         {
             //this.transform.Translate(Vector3.back * attacked * Time.deltaTime); // 적에게 닿은후 캐릭터의 위치가 뒤로 밀림 attacked값을 바꾸면 밀린정도를 바꿀수있음
             CharacterAnimation.DamageAnimation(); // 체력깎임 애니메이션 실행
             HP--;
-            CharacterMove.runSpeed = CharacterMove.runSpeed /2.0f;
+            CharacterMove.runSpeed = CharacterMove.runSpeed / 2.0f;
             Invoke("DamagedEvent3", 1.5f);
         }
         else
@@ -180,7 +227,7 @@ public class ObjEat : MonoBehaviour,IObjToCharactor { // ,IObjToCharactor
     {
         Debug.Log("체력닳음");
         Debug.Log("HP " + HP + " 개");
-        CharacterMove.runSpeed = CharacterMove.runSpeed *2.0f;
+        CharacterMove.runSpeed = CharacterMove.runSpeed * 2.0f;
     }
 
     public void GameOver() // 캐릭터가 쓰러진상태로 유지시켜주는 함수
@@ -192,4 +239,41 @@ public class ObjEat : MonoBehaviour,IObjToCharactor { // ,IObjToCharactor
     {
         return false;
     }
+
+    IEnumerator InvincibleCoroutine()
+    {
+        while (Invincible)
+        {
+            yield return new WaitForSeconds(invincibleTime);
+            invincibleTime = 0;
+            Invincible = false;
+            print(Invincible);
+        }
+        //yield return null;
+    }
+
+    IEnumerator ShieldCoroutine()
+    {
+        while (Shield)
+        {
+            yield return new WaitForSeconds(shieldTime);
+            shieldTime = 0;
+            Shield = false;
+            print(Shield);
+        }
+        //yield return null;
+    }
+
+    IEnumerator MagnetCoroutine()
+    {
+        while (Magnet)
+        {
+            yield return new WaitForSeconds(magnetTime);
+            magnetTime = 0;
+            Magnet = false;
+            print(Magnet);
+        }
+    }
 }
+
+
