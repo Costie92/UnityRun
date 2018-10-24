69d6ea467a4ab1d6649afe3e35edca1a3ab2f36e
HGS <cats.c.cats@gmail.com>
2018-10-24 20:01:40 +0900

    no message

---
+++ Assets/Scripts/CameraPosition.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPosition : MonoBehaviour
+{
+
+    GameObject UnityChan;
+
+    // Use this for initialization
+    void Start()
+    {
+        this.UnityChan = GameObject.Find("unitychan");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(this.UnityChan.transform.position.x, 7.5f, this.UnityChan.transform.position.z - 5f);
+    }
+}
