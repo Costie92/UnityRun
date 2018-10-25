c8d2d93fef5c0808c7b76e136451fb0c2cde5602
HGS <cats.c.cats@gmail.com>
2018-10-25 16:21:02 +0900

    anispeed

--- Assets/Scripts/CharacterAnimation.cs
+++ Assets/Scripts/CharacterAnimation.cs
@@ -30,7 +30,7 @@ public class CharacterAnimation : MonoBehaviour // 캐릭터의 애니메이션 
     // Update is called once per frame
     void Update()
     {
-
+        InvincibleRunAnimation();
     }
 
     /* ObjEat으로 옮김
@@ -91,8 +91,19 @@ public class CharacterAnimation : MonoBehaviour // 캐릭터의 애니메이션 
         this.GetComponent<CapsuleCollider>().center = new Vector3(0, 0.75f, 0); // 캐릭터 콜라이더 중심 옮기기
         this.GetComponent<CapsuleCollider>().height = 1.5f; // 캐릭터 콜라이더 높이 바꾸기
     }
+    void InvincibleRunAnimation()
+    {
+        if(ObjEat.Invincible == true)
+        {
+            animator.SetFloat("RunSpeed", 2.0f);
+        }
+        else
+        {
+            animator.SetFloat("RunSpeed", 1.0f);
+        }
+    }
 
-    public void GameOver() // 캐릭터가 쓰러진상태로 유지시켜주는 함수
+public void GameOver() // 캐릭터가 쓰러진상태로 유지시켜주는 함수
     {
         UIMgr.ShowResult(); // 캐릭터 시간멈춤 (캐릭터가 쓰러진 모습 상태로 정지함)
     }
