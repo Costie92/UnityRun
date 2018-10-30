1523ac2c70ee96f1e0a740ca148b2ca770f6b2aa
hcp <suna.hing@gmail.com>
2018-10-31 00:53:10 +0900

    no message

---
+++ Assets/MapLoadingPart/02.Scripts/boss/BossMapMgr.cs
@@ -0,0 +1,169 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace hcp
+{
+    public enum E_BOSSPATTERN
+    {
+        ONCE_FIREBALL=0,
+        BREATH,
+        METEOR
+    }
+
+    public class BossMapMgr : SingletonTemplate<BossMapMgr>
+    {
+        public GameObject chunk;
+        public float moveSpeed = 10.0f;
+        List<BossMapST> mapSTList = new List<BossMapST>();
+        float chunkMargin;
+
+        protected override void Awake()
+        {
+            base.Awake();
+            chunkMargin = chunk.GetComponent<Renderer>().bounds.size.z;
+
+            for (int i = 0; i < Constants.wantToShowNumOfChunks; i++)
+            {
+                BossMapST bst = new BossMapST();
+                mapSTList.Add(bst);
+            }
+        }
+        private void Start()
+        {
+            MapAndObjPool.GetInstance().ChunkPoolInit(Constants.wantToShowNumOfChunks);
+
+            InitMapST();
+        }
+
+        private void Update()
+        {
+            //패턴 따라 넣어주는 처리.
+            mapSTList.MoveAllInList(moveSpeed);
+            BossChunkLoading();
+        }
+
+        void BossChunkLoading()
+        {
+            BossMapST bst = mapSTList.FindChunkShouldBeRemoved(chunkMargin);
+            if (bst == null) return;
+
+            bst.TurnInObjs();
+            bst.MoveChunk(mapSTList.GetNewCreatePoint(chunkMargin));
+
+            ServeBossPattern(bst);
+        }
+
+        void ServeBossPattern(BossMapST newOne)
+        {
+            //큐로 받은 패턴을 뽑아서
+            //맞게 넣어줌
+
+        }
+
+
+        void InitMapST()
+        {
+            float startPoint = -1*Constants.wantToShowNumOfChunksInBehind;
+            for (int i = 0; i < Constants.wantToShowNumOfChunks; i++)
+            {
+                GameObject chunk = 
+                MapAndObjPool.GetInstance().GetChunkInPool();
+                if (chunk == null) ErrorManager.SpurtError("청크가 널임");
+                if (startPoint == 0)
+                    chunk.transform.position = Vector3.zero;
+                else
+                    chunk.transform.position = Vector3.forward* startPoint * chunkMargin;
+
+                mapSTList[i].chunk = chunk;
+                mapSTList[i].chunk.SetActive(true);
+                startPoint++;
+            }
+        }
+
+    }
+
+
+    public static class MyExtendMethodForListBossMapST
+    {
+        public static void MoveAllInList(this List<BossMapST> list , float moveSpeed)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                if(list[i].IsDisabled() == false)
+                list[i].MoveAll(moveSpeed);
+            }
+        }
+        public static BossMapST FindChunkShouldBeRemoved(this List<BossMapST> list, float chunkMargin)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i].IsDisabled()==false && list[i].IsShouldBeRemoved(chunkMargin))
+                    return list[i];
+            }
+            return null;
+        }
+        public static float GetNewCreatePoint(this List<BossMapST> list, float chunkMargin)
+        {
+            float last = 0;
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i].IsDisabled()) continue;
+
+                if (list[i].chunk.transform.position.z > last)
+                    last = list[i].chunk.transform.position.z;
+            }
+            return last + chunkMargin;
+        }
+    }
+
+    public class BossMapST
+    {
+        public GameObject chunk;
+        public List<GameObject> objs = new List<GameObject>();
+
+        public void MoveAll(float moveSpeed)
+        {
+            chunk.transform.Translate(Vector3.back * Time.deltaTime * moveSpeed);
+
+            for (int i = 0; i < objs.Count; i++)
+            {
+                objs[i].transform.Translate(Vector3.back * Time.deltaTime * moveSpeed);
+            }
+        }
+        public bool IsShouldBeRemoved(float chunkMargin)
+        {
+            if (chunk.transform.position.z <= -1 * ((Constants.wantToShowNumOfChunksInBehind +1) * chunkMargin))
+                return true;
+            else return false;
+        }
+        public void TurnInObjs()
+        {
+            for (int i = 0; i < objs.Count; i++)
+            {
+                MapAndObjPool.GetInstance().TurnInPoolObj(objs[i]);
+            }
+            objs.Clear();
+        }
+        public bool IsDisabled()
+        {
+            if (chunk==null)
+                return true;
+            else return false;
+        }
+        public void MoveChunk(float pos)
+        {
+            chunk.transform.position = Vector3.forward * pos;
+        }
+        public void ObjIn(int spawnPointNum, GameObject obj)
+        {
+            if (obj==null||IsDisabled()) ErrorManager.SpurtError("업음");
+
+            Transform spawnPoint = chunk.transform.GetChild(3) //3은 굉장히 청크의 자식순서에 종속적인 값
+                .GetChild(spawnPointNum);   
+
+            obj.transform.SetPositionAndRotation(spawnPoint.position, spawnPoint.rotation);
+            obj.SetActive(true);
+            objs.Add(obj);
+        }
+    }
+}
