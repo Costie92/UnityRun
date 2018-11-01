52e4655c31ba34d1608300bbca90a1b9d534709a
hcp <suna.hing@gmail.com>
2018-11-01 20:54:44 +0900

    no message

--- Assets/MapLoadingPart/02.Scripts/item/ItemCtrl.cs
+++ Assets/MapLoadingPart/02.Scripts/item/ItemCtrl.cs
@@ -8,13 +8,14 @@ namespace hcp
     {
         protected IObjToCharactor objToCharactor;
         MeshRenderer childModelRenderer;
+        GameObject childModel;
         
         protected ItemST itemST;
 
         protected virtual void Awake()
         {
             itemST = new ItemST();
-
+            childModel = transform.Find("childModel").gameObject;
             objToCharactor = GameObject.FindGameObjectWithTag("PLAYER").GetComponent<IObjToCharactor>();
             childModelRenderer = transform.Find("childModel").gameObject.GetComponent<MeshRenderer>();
         }
@@ -26,12 +27,14 @@ namespace hcp
                 objToCharactor.GetItem(itemST);
 
                 childModelRenderer.enabled = false;
+                childModel.GetComponent<Collider>().enabled = false;
             }
         }
         protected virtual void OnEnable() {
             if(childModelRenderer.enabled.Equals(false))
             {
                 childModelRenderer.enabled = true;
+                childModel.GetComponent<Collider>().enabled = true;
             }
         }
     }
