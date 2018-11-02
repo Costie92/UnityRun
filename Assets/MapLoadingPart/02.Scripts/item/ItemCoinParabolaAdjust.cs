ebd3efb63f813245cbc9ede43dc9dc62e5fa90ed
hcp <suna.hing@gmail.com>
2018-11-03 01:17:38 +0900

    no message

--- Assets/MapLoadingPart/02.Scripts/item/ItemCoinParabolaAdjust.cs
+++ Assets/MapLoadingPart/02.Scripts/item/ItemCoinParabolaAdjust.cs
@@ -5,9 +5,27 @@ using UnityEngine;
 public class ItemCoinParabolaAdjust : MonoBehaviour {
     public GameObject huddle;
     public GameObject fire;
-   
+    public GameObject[] coinObjs;
+
+    float coinLineHighPointY;
+    float jumpStartPointZ;
+    float a;
+    Vector3 pos;
+
     private void Awake()
     {
+        coinLineHighPointY = 3;
+        jumpStartPointZ = 3;    //점프 시작위치에 맞춰서 설정
+
+        a = -coinLineHighPointY / (jumpStartPointZ * jumpStartPointZ);
+
+        pos = new Vector3();
+
+        coinObjs = new GameObject[10];
+        for (int i = 0; i < 10; i++)
+        {
+            coinObjs[i] = transform.GetChild(i).gameObject;
+        }
         huddle = transform.Find("Huddle").gameObject;
         fire = transform.Find("Fire").gameObject;
     }
@@ -34,7 +52,25 @@ public class ItemCoinParabolaAdjust : MonoBehaviour {
         }
     }
     private void OnDisable()
-    {
+    {  
+        transform.position = Vector3.zero;
+        float x = 0;
+        float zp = 4.5f;
+        float y=0;
+        pos.x = x;
+        
+            for (int i = 0; i < coinObjs.Length; i++)
+            {
+                y = a * (zp*zp) + coinLineHighPointY;
+                if (y < 0) y = 0;
+
+                pos.y = y;
+                pos.z = zp;
+
+                coinObjs[i].transform.position = pos;
+                zp -= 1;
+            }
+
         huddle.transform.position = transform.position;
         huddle.transform.rotation = transform.rotation;
     }
