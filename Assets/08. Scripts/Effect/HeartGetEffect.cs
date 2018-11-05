a5d2b9cab1346d20003a8653711f9db96957dad1
HGS <cats.c.cats@gmail.com>
2018-11-05 15:29:29 +0900

    SkillEffect

---
+++ Assets/08. Scripts/Effect/HeartGetEffect.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeartGetEffect : MonoBehaviour {
+
+    GameObject UnityChan;
+
+    // Use this for initialization
+    void Start()
+    {
+        this.UnityChan = GameObject.Find("unitychan");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        EffectCreat();
+        Destroy(this, 3.0f);
+    }
+
+    void EffectCreat()
+    {
+            transform.position = new Vector3(this.UnityChan.transform.position.x, this.UnityChan.transform.position.y + 1.5f, this.UnityChan.transform.position.z - 0.5f);
+    }
+}
---
+++ Assets/08. Scripts/Effect/SkillEffectM.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillEffectM : MonoBehaviour {
+
+    GameObject UnityChan;
+
+    // Use this for initialization
+    void Start () {
+        this.UnityChan = GameObject.Find("unitychan");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        EffectCreat();
+    }
+
+    void EffectCreat()
+    {
+        if(ObjEat.Magnet == true)
+        {
+            transform.position = new Vector3(this.UnityChan.transform.position.x, this.UnityChan.transform.position.y, this.UnityChan.transform.position.z+0.5f);
+        }
+        else if(ObjEat.Magnet == false)
+        {
+            transform.position = new Vector3(0,0,0);
+        }
+    }
+}
